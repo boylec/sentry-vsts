8ac3d6e9f48c7fcb3c8516d46a42dcf44715a657
Casey Boyle <casey.boyle@vision2systems.com>
2017-09-01 14:16:51 -0500

    Initial read me

---
+++ README.txt
@@ -0,0 +1,26 @@
+===============
+Sentry-Vsts
+===============
+
+This is a plugin that can be installed on a Sentry.io server which is intended
+to allow a Sentry.io user to integrate issues reported in Sentry.io with Visual
+Studio Team Services work items.
+
+
+Installation
+============
+
+Once the plugin is on PyPi (which it currently isn't), one would install and use
+this plugin just install it via ``pip``::
+
+  pip install sentry_vsts
+
+
+Until then:
+
+  pip install -e git+https://github.com/boylec/sentry_vsts
+
+Once the plugin is installed, it will be configurable on a per-project basis
+within the Sentry.io UI. (Choose a project to integrate, and then go to project
+settings in the top right, and find the Visual Studio Team Services Plugin.
+Click configure and fill in configuration fields as needed.)
---
+++ setup.py
@@ -0,0 +1,32 @@
+"""This module installs the Sentry.io VSTS integration plugin"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sentry_vsts',
+    version='0.0.1',
+    author='Casey Boyle',
+    author_email='boylec@live.com',
+    url='https://github.com/boylec/sentry-vsts',
+    description='A Sentry.io plugin for Visual Studio Team Services integration',
+    keywords='sentry-vsts sentry vsts visual studio team services sentry.io',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'sentry>=4.6.0',
+        'requests>=0.12.1',
+    ],
+    dependency_links=[],
+    license='MIT',
+    include_package_data=True,
+    entry_points={
+        'sentry.plugins':['vsts=src.plugin:VstsPlugin']
+    },
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
